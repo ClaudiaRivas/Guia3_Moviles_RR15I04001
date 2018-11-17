2d9d329c0d283984698065beda42be3f2b73525c
ClaudiaRivas <42727618+ClaudiaRivas@users.noreply.github.com>
2018-11-17 08:46:01 -0600

    Add files via upload

---
+++ src/main/java/org/dev4u/hv/guia3_ejemplo/Mensaje.java
@@ -0,0 +1,11 @@
+package org.dev4u.hv.guia3_ejemplo;
+
+public class Mensaje {
+    public String mensaje;
+    public String hora;
+
+    public Mensaje(String mensaje, String hora) {
+        this.mensaje = mensaje;
+        this.hora = hora;
+    }
+}
